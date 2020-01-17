8a64bf805ee479f6a3538a817ba4d0a518c147a8
Ronnie Kwok <ronniek@gmail.com>
2020-01-17 15:37:53 +0800

    Turn on debug mode in mvn package

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,7 +27,7 @@ fi
 CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo "CODE_DIR"
 echo $CODE_DIR
-$DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-8 mvn -q -DskipTests package
+$DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-8 mvn -X -DskipTests package
 
 cp $CODE_DIR/target/*.jar $CODE_DIR/docker/carts
 
